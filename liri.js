9c3a146a6b6e383d930978dbbdca2cb7d9e71c19
richardluong127 <richardluong127@gmail.com>
2019-01-08 21:42:49 -0800

    working

--- liri.js
+++ liri.js
@@ -1,20 +1,11 @@
 var Spotify = require('node-spotify-api');
 var request = require('request');
-// request('http://www.google.com', function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
 var moment = require('moment');
-var dotenv = require('dotenv').config;
-var bands = require('bandsintown');
-//var spotify = new Spotify(keys.spotify);
-
+var fs = require('fs');
 
 var a = process.argv[2];
 var b = process.argv[3];
 
-var omdb = "http://www.omdbapi.com/?apikey=trilogy&";
 var bit = "https://rest.bandsintown.com/artists/" + b + "/events?app_id=codingbootcamp";
 var spotify = new Spotify({
   id: "f11d8b254cc94fb68c347ba592b526b5",
@@ -22,7 +13,7 @@ var spotify = new Spotify({
 });
 
 if (a === "concert-this") {
-  request(bit, function(err, res) {
+  request(bit, function (err, res) {
     if (err) throw err;
     var data = JSON.parse(res.body);
     console.log(data[0].venue.name);
@@ -30,16 +21,17 @@ if (a === "concert-this") {
     console.log(moment(data[0].datetime).format("L"));
   });
 };
+
 if (!process.argv[3]) {
-      b = "Ace of Base";
-    }
+  b = "Ace of Base";
+}
 if (a === "spotify-this-song") {
 
   spotify.search({ type: 'track', query: b }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-    
+
     console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name);
     console.log(data.tracks.items[0].name);
     console.log(data.tracks.items[0].preview_url);
@@ -69,4 +61,53 @@ if (a === "movie-this") {
   });
 };
 
+if (a === "do-what-it-says") {
+  fs.readFile('random.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    var dataArr = data.split(',');
+    if (!dataArr[1]) {
+      dataArr[1] = "Ace of Base";
+    }
+    else if (dataArr[0] === "spotify-this-song") {
+      spotify.search({ type: 'track', query: dataArr[1] }, function (err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name);
+        console.log(data.tracks.items[0].name);
+        console.log(data.tracks.items[0].preview_url);
+        console.log(data.tracks.items[0].album.name);
+      });
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + b + "&y=&plot=short&apikey=trilogy";
+    if (!dataArr[1]) {
+      queryUrl = "http://www.omdbapi.com/?t=" + "Mr.Nobody" + "&y=&plot=short&apikey=trilogy";
+    }
+    if (dataArr[0] === "movie-this") {
+      request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
 
+          var jsonData = JSON.parse(body);
+
+          console.log("Title: " + jsonData.Title);
+          console.log("Released: " + jsonData.Year);
+          console.log("IMDB Rating: " + jsonData.imdbRating);
+          console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+          console.log("Produced In: " + jsonData.Country);
+          console.log("Language(s): " + jsonData.Language);
+          console.log("Plot: " + jsonData.Plot);
+          console.log("Actors: " + jsonData.Actors);
+        }
+      });
+    };
+    if (dataArr[0] === "concert-this") {
+      request("https://rest.bandsintown.com/artists/" + dataArr[1] + "/events?app_id=codingbootcamp", function (err, res) {
+        if (err) throw err;
+        var data = JSON.parse(res.body);
+        console.log(data[0].venue.name);
+        console.log(data[0].venue.city + ", " + data[0].venue.country);
+        console.log(moment(data[0].datetime).format("L"));
+      });
+    };
+  });
+};
